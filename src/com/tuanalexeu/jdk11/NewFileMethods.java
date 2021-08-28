34a4c4e122dc770cdd3c2900b4c6eac49b2d2bf5
tuanalexeu <alekseytyan45@gmail.com>
2021-08-28 19:35:12 +0300

    New NIO API in JDK11

---
+++ src/com/tuanalexeu/jdk11/NewFileMethods.java
@@ -0,0 +1,17 @@
+package com.tuanalexeu.jdk11;
+
+/**
+ * Additionally, it's now easier to read and write Strings from files.
+ * We can use the new readString and writeString static methods from the Files class
+ */
+public class NewFileMethods {
+
+    public static void readString() {
+        // String testString = Files.readString(tempDir);
+    }
+
+    public static void writeString() {
+        // Path filePath = new Files.writeString(Files.createTempFile(tempDir, "test", ".txt"), "Sample text");
+
+    }
+}
