1f2b3b5df4494feaf82e4ea9fe2b984a5425bb76
tuanalexeu <alekseytyan45@gmail.com>
2021-08-30 10:12:25 +0300

    New predicate methods in JDK 11 (TODO)

---
+++ src/main/java/com/tuanalexeu/jdk11/NewPredicateMethods.java
@@ -0,0 +1,13 @@
+package com.tuanalexeu.jdk11;
+
+public class NewPredicateMethods {
+
+    /**
+     * A static not method has been added to the Predicate interface.
+     * We can use it to negate an existing predicate, much like the negate method.
+     */
+    public static void not() {
+
+    }
+
+}
