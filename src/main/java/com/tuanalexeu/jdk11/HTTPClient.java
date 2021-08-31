d55da23591c9eb13bfe04a34bb6b938d6f68aa10
tuanalexeu <alekseytyan45@gmail.com>
2021-08-31 09:57:38 +0300

    New built-in HTTP Client in JDK11 (TODO)

---
+++ src/main/java/com/tuanalexeu/jdk11/HTTPClient.java
@@ -0,0 +1,17 @@
+package com.tuanalexeu.jdk11;
+
+public class HTTPClient {
+
+    /**
+     * The new HTTP client from the java.net.http package was introduced in Java 9.
+     * It has now become a standard feature in Java 11.
+     */
+    public static void newHttpClient() {
+        // TODO define basic aspects of a new HTTP client
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
